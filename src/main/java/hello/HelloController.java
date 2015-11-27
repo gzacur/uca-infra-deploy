4ed6ba425c08a7f059156bf765ab640cdf49b228
Guillermo Zacur <zakyur@gmail.com>
2015-11-27 17:43:16 -0300

    agregando el proyecto

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index(@RequestParam(name = "name", defaultValue = "World") String name) {
+
+		return "Hello " + name;
+
+	}
+
+}
